4723ef02a5e531e9b479f2300842136746a4a792
angelovangel <aangeloo@gmail.com>
2022-02-09 07:41:44 +0100

    move logit in lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+
+pub fn get_n_bases(seq: &[u8]) -> i64 {
+    let mut n = 0;
+    for s in seq {
+        if s == &78u8 || s == &110u8 {
+            n += 1;
+        }
+    }
+    n
+}
+
+pub fn get_qual_bases(q: &[u8], qx: u8) -> i64 {
+    let mut n = 0;
+    for item in q {
+        if *item >= qx {
+            n += 1
+        }
+    }
+    n
+}
+
+// to get mean of q scores from a record - first convert to prob, calc mean, then back to phred
+// this fn reads phred and converts to probs and returns their sum
+//
+pub fn qscore_probs(q: &[u8]) -> f32 {
+    let mut qprob_sum = 0.0;
+    for &item in q.iter() {
+        let phred = *&item as f32 - 33.0;
+        let prob = 10.0_f32.powf(-phred / 10.0);
+        qprob_sum += prob
+    }
+    qprob_sum
+}
+
+pub fn get_nx(numbers: &mut [i64], fraction: f32) -> i64 {
+    numbers.sort_unstable();
+
+    // half of the bases
+    let halfsum = numbers.iter().sum::<i64>() as f32 * fraction; // f32 * f32
+
+    // cumsum of the sorted vector
+    let cumsum = numbers
+        .iter()
+        .scan(0, |sum, i| {
+            *sum += i;
+            Some(*sum)
+        })
+        .collect::<Vec<_>>();
+    let n50_index = cumsum.iter().position(|&x| x > halfsum as i64).unwrap();
+
+    numbers[n50_index]
+}
--- src/main.rs
+++ src/main.rs
@@ -4,57 +4,8 @@ use kseq::parse_path;
 extern crate clap;
 use clap::{App, Arg, ArgGroup};
 
-fn get_n_bases(seq: &[u8]) -> i64 {
-    let mut n = 0;
-    for s in seq {
-        if s == &78u8 || s == &110u8 {
-            n += 1;
-        }
-    }
-    n
-}
+use faster2;
 
-fn get_qual_bases(q: &[u8], qx: u8) -> i64 {
-    let mut n = 0;
-    for item in q {
-        if *item >= qx {
-            n += 1
-        }
-    }
-    n
-}
-
-// to get mean of q scores from a record - first convert to prob, calc mean, then back to phred
-// this fn reads phred and converts to probs and returns their sum
-//
-fn qscore_probs(q: &[u8]) -> f32 {
-    let mut qprob_sum = 0.0;
-    for &item in q.iter() {
-        let phred = *&item as f32 - 33.0;
-        let prob = 10.0_f32.powf(-phred / 10.0);
-        qprob_sum += prob
-    }
-    qprob_sum
-}
-
-fn get_nx(numbers: &mut [i64], fraction: f32) -> i64 {
-    numbers.sort_unstable();
-
-    // half of the bases
-    let halfsum = numbers.iter().sum::<i64>() as f32 * fraction; // f32 * f32
-
-    // cumsum of the sorted vector
-    let cumsum = numbers
-        .iter()
-        .scan(0, |sum, i| {
-            *sum += i;
-            Some(*sum)
-        })
-        .collect::<Vec<_>>();
-    let n50_index = cumsum.iter().position(|&x| x > halfsum as i64).unwrap();
-
-    numbers[n50_index]
-}
 
 fn main(){
 
@@ -101,7 +52,7 @@ fn main(){
 
     } else if matches.is_present("qual") {
         while let Some(record) = records.iter_record().unwrap() {
-            let mean_prob = qscore_probs( record.qual().as_bytes() ) / record.len() as f32;
+            let mean_prob = faster2::qscore_probs( record.qual().as_bytes() ) / record.len() as f32;
             println!("{:.4}", -10.0 * mean_prob.log10()); // convert to phred score
         }
         process::exit(0);
@@ -119,15 +70,15 @@ fn main(){
             len_vector.push(len);
             reads += 1;
             bases += record.len() as i64;
-            num_n += get_n_bases(record.seq().as_bytes() );
-            qual20 += get_qual_bases(record.qual().as_bytes(), 53); // 33 offset
+            num_n += faster2::get_n_bases(record.seq().as_bytes() );
+            qual20 += faster2::get_qual_bases(record.qual().as_bytes(), 53); // 33 offset
             //qual30 += get_qual_bases(record.qual().as_bytes(), 63);
         
         }
 
         let q20 = qual20 as f64 / bases as f64 * 100.0;
         //let q30 = qual30 as f64 / bases as f64 * 100.0;
-        let n50 = get_nx(&mut len_vector, 0.5);
+        let n50 = faster2::get_nx(&mut len_vector, 0.5);
         let min_len = len_vector.iter().min().unwrap();
         let max_len = len_vector.iter().max().unwrap();
 
