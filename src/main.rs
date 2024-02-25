5eb3dfc82cbd5a28167dd09bf23b176b392d80ae
angelovangel <aangeloo@gmail.com>
2024-02-25 09:59:24 +0100

    add elapsed time

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use kseq::parse_path;
 
 extern crate clap;
 use clap::{App, Arg, ArgGroup};
-use indicatif::{HumanCount, ProgressBar};
-use std::time::Duration;
+use indicatif::{HumanCount, HumanDuration, ProgressBar};
+use std::time::{Duration, Instant};
 use owo_colors::OwoColorize;
 
 use faster2;
@@ -66,7 +66,8 @@ fn main(){
             .index(1))
         .group(ArgGroup::with_name("group").required(true).args(&["table", "len", "qual", "gc", "nx", "qyield"]))
         .get_matches();
-	
+
+	let start = Instant::now();
     let infile = matches.value_of("INPUT").unwrap();
 	let mut records = parse_path(infile).unwrap();
 	
@@ -158,7 +159,14 @@ fn main(){
             num_n += faster2::get_n_bases(record.seq().as_bytes() );
             qual20 += faster2::get_qual_bases(record.qual().as_bytes(), 53); // 33 offset
             //qual30 += get_qual_bases(record.qual().as_bytes(), 63);
-            let message = format!("Processed reads: {}", HumanCount(reads as u64).to_string().green());
+            //let speed = (reads as u64).checked_div(start.elapsed().as_secs()).ok_or(1);
+            
+            let message = format!(
+                "Processed reads: {} ({})", 
+                HumanCount(reads as u64).to_string().green(),
+                HumanDuration(start.elapsed()) 
+                //speed.unwrap_or(0)
+            );
             pb.set_message(message);
         
         }
